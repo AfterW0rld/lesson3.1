de2b276e3736e86af9de419f0834c1adde91cb5a
AfterW0rld <pasha_belovol@ukr.net>
2019-02-26 22:16:30 +0200

    No homework.

---
+++ src/com/company/Main.java
@@ -0,0 +1,59 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double x, y, a, b;
+        a = 1;
+        b = 2;
+        for (x = a; x < b; x+=0.1){
+            if (Math.cos(x) == 0){
+                break;
+            } else {
+                y = Math.tan(x);
+                System.out.println("y = " + y);
+                System.out.println("x = " + x);
+            }
+        }
+//          double x = 1.0,ch=x,p=x,  fz=1, f, sign=+1, sum=x ;
+//          for (f = 3 ; f <=13; f+=2){
+//              sign *= -1;
+//              // ch = Math.pow(x, f);
+//              ch = ch * x * x;
+//              fz = fz * f * (f-1);
+//              p = ch / fz;
+//              sum +=p*sign;
+//
+//          }
+//        System.out.println(sum);
+
+
+
+//        double a, b;
+//        for (a=2; a <= 20; a++)
+//        {
+//            b = Math.sin(a);
+//            System.out.println(b);
+//        }
+
+
+
+
+//        int a, b, c  ;
+//        b = 7;
+//        for (a=1;a<=9;a++)
+//        {
+//            c = a*b;
+//            System.out.println(a + " * " + b + " = " + c);
+//        }
+
+
+
+        // int a, b, c;
+        // a = 123456789;
+        //for (b=9 ; b <= 81 ; b = b +9){
+        //    c = a*b;
+        //    System.out.println(c);
+        // }
+    }
+}
